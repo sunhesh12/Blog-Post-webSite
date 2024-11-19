cfd9b284912bbaeb3d60cc34f8483e21c112290d
sunhesh12 <hsdbandaranayake@gmail.com>
2024-11-19 22:46:31 +0530

    new

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -22,18 +22,21 @@
     <div class="container">
       <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
-          <div class="col-4 pt-1">
-            <a class="text-muted" href="#">Subscribe</a>
-          </div>
+          <!-- <div class="col-4 pt-1">
+            <a class="text-muted " href="#">Subscribe</a>
+          </div> -->
           <div class="col-4 text-center">
-            <a class="blog-header-logo text-dark" href="#">Large</a>
+            <a class="blog-header-logo text-dark text-decoration-none " href="#">SECRET WORLD</a>
           </div>
           <div class="col-4 d-flex justify-content-end align-items-center">
             <a class="text-muted" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3">
+                <circle cx="10.5" cy="10.5" r="7.5"></circle>
+                <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
+              </svg>
             </a>
-            <a class="btn btn-sm btn-outline-secondary mr-2" href="{{route('login')}}">Log In</a>
-            <a class="btn btn-sm btn-outline-secondary" href="{{route('register')}}">Sign up</a>
+            <a class="btn btn-sm btn-outline-secondary ms-2" href="{{ route('login') }}">Log In</a> <!-- Margins set by ms-4 -->
+            <a class="btn btn-sm btn-outline-secondary ms-2" href="{{ route('register') }}">Sign up</a>
           </div>
         </div>
       </header>
@@ -44,9 +47,9 @@
 
     
     <footer class="blog-footer">
-      <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/">@Heshan Santhush</a>.</p>
       <p>
-        <a href="#">Back to top</a>
+        <a href="{{Route('welcome')}}">Back to Home</a>
       </p>
     </footer>
 
